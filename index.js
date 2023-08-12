9d68c7b2ad82d5bca0f246f25378efc572c76f3c
Dhruv <dhruv.singh_cs21@gla.ac.in>
2023-08-12 19:48:23 +0530

    some changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const mongoose = require('mongoose');
 
 
 app.set('view engine', 'ejs');
@@ -9,10 +10,8 @@ app.use(express.static(path.join(__dirname, '/public')));
 const port = 8000 || 3000;
 
 
-const mongoose = require('mongoose');
 // important for restAPI
 const methodOverride = require('method-override');
-
 const passport = require('passport');
 
 
@@ -23,7 +22,6 @@ const JourneyDB = require('./models/JourneyDB');
 
 
 const session = require('express-session');
-const { render } = require('ejs');
 // body parser
 app.use(express.urlencoded({ extended: true })); //for form data
 app.use(methodOverride('_method'))
@@ -85,110 +83,6 @@ app.get('/logout', function (req, res, next) {
 /* 
 the journey api routes
 
-// // // */
-// const dummyJourneys = [
-//     {
-//         name: "John Doe",
-//         date: new Date("2023-08-15"),
-//         fromLocation: "New York",
-//         toLocation: "Los Angeles",
-//         email: "user1@example.com",
-//         mobile: "123-456-7890",
-//         created: new Date("2023-08-10"),
-//         updated: new Date("2023-08-10")
-//     },
-//     {
-//         name: "Jane Smith",
-//         date: new Date("2023-09-05"),
-//         fromLocation: "Chicago",
-//         toLocation: "Miami",
-//         email: "user2@example.com",
-//         mobile: "987-654-3210",
-//         created: new Date("2023-08-11"),
-//         updated: new Date("2023-08-11")
-//     },
-//     {
-//         name: "Michael Johnson",
-//         date: new Date("2023-08-25"),
-//         fromLocation: "Seattle",
-//         toLocation: "San Francisco",
-//         email: "user3@example.com",
-//         mobile: "555-123-4567",
-//         created: new Date("2023-08-09"),
-//         updated: new Date("2023-08-09")
-//     },
-//     {
-//         name: "Emily Davis",
-//         date: new Date("2023-08-18"),
-//         fromLocation: "Boston",
-//         toLocation: "Orlando",
-//         email: "user4@example.com",
-//         mobile: "222-333-4444",
-//         created: new Date("2023-08-10"),
-//         updated: new Date("2023-08-10")
-//     },
-//     {
-//         name: "William Johnson",
-//         date: new Date("2023-08-22"),
-//         fromLocation: "Denver",
-//         toLocation: "Las Vegas",
-//         email: "user5@example.com",
-//         mobile: "555-999-8888",
-//         created: new Date("2023-08-10"),
-//         updated: new Date("2023-08-10")
-//     },
-//     {
-//         name: "Linda Anderson",
-//         date: new Date("2023-08-27"),
-//         fromLocation: "Dallas",
-//         toLocation: "Houston",
-//         email: "user6@example.com",
-//         mobile: "777-444-1111",
-//         created: new Date("2023-08-10"),
-//         updated: new Date("2023-08-10")
-//     },
-//     {
-//         name: "Robert White",
-//         date: new Date("2023-08-20"),
-//         fromLocation: "Atlanta",
-//         toLocation: "New Orleans",
-//         email: "user7@example.com",
-//         mobile: "999-888-5555",
-//         created: new Date("2023-08-10"),
-//         updated: new Date("2023-08-10")
-//     },
-//     {
-//         name: "Maria Rodriguez",
-//         date: new Date("2023-08-29"),
-//         fromLocation: "Phoenix",
-//         toLocation: "Salt Lake City",
-//         email: "user8@example.com",
-//         mobile: "123-555-7777",
-//         created: new Date("2023-08-10"),
-//         updated: new Date("2023-08-10")
-//     },
-//     {
-//         name: "Daniel Lee",
-//         date: new Date("2023-09-10"),
-//         fromLocation: "Minneapolis",
-//         toLocation: "Nashville",
-//         email: "user9@example.com",
-//         mobile: "888-333-2222",
-//         created: new Date("2023-08-10"),
-//         updated: new Date("2023-08-10")
-//     },
-//     {
-//         name: "Sophia Martinez",
-//         date: new Date("2023-09-03"),
-//         fromLocation: "Philadelphia",
-//         toLocation: "Washington, D.C.",
-//         email: "user10@example.com",
-//         mobile: "666-777-8888",
-//         created: new Date("2023-08-10"),
-//         updated: new Date("2023-08-10")
-//     }
-// ];
-
 // JourneyDB.insertMany(dummyJourneys);
 
 // You can use this dummy data to insert into your MongoDB database using Mongoose.
@@ -212,27 +106,30 @@ the journey api routes
 // app.get('/')
 // show route 1
 // enter details route 
-// 
-app.get('/route', (req, res) => {
-    res.render('journey/journey');
+*/
+
+app.get('/route',async (req,res)=>{
+    const journeyObj = await JourneyDB.find({});
+    res.render('journey/journey.ejs',{journeyObj});
 })
-app.get('/route/add', (req, res) => {
-    if (req.isAuthenticated()) {
-        res.render('journey/add.ejs');
-    } else {
-        res.render('/register');
-    }
-})
-// third route post request
-app.post('/route', async (req, res) => {
-    let { name, email, mobile, fromLocation, toLocation, date } = req.body;
-    await JourneyDB.create({ name, email, mobile, fromLocation, toLocation, date });
-    res.redirect('/route');
-})
+// app.get('/route/add', (req, res) => {
+//     if (req.isAuthenticated()) {
+//         res.render('journey/add.ejs');
+//     } else {
+//         res.redirect('/login');
+//     }
+
+// })
+// // third route post request
+// app.post('/route', async (req, res) => {
+//     let { name, email, mobile, fromLocation, toLocation, date } = req.body;
+//     await JourneyDB.create({ name, email, mobile, fromLocation, toLocation, date });
+//     res.redirect('/route');
+// })
 // show a single route
 app.get('/route/:id', async (req, res) => {
     let { id } = req.params;
-    let found = await Product.findById(id);
+    let found = await JourneyDB.findById(id);
     // console.log(found);
     res.render('journey/show', { found })
 })
@@ -260,31 +157,9 @@ app.get('/route/:id', async (req, res) => {
 // // route for deleteing shit
 // router.delete('/products/:id',async (req,res)=>{
 //     let {id} = req.params;
-//     await Product.findByIdAndDelete(id);
+ //     await Product.findByIdAndDelete(id);
 //     res.redirect('/products');
 // })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // route 1 
 // main index route
@@ -292,12 +167,11 @@ app.get('/', async (req, res) => {
     if (req.isAuthenticated()) {
         // The user is logged in
         let journeyObj = await JourneyDB.find({});
-        res.render('userHome', { journeyObj })
+      res.render('userHome',{journeyObj});
 
     } else {
-        let journeyObj = await JourneyDB.find({});
-        res.render('home', { journeyObj })
-        // The user is logged out
+        res.render('home')
+        // The user is logged out 
     }
 })
 // 
@@ -305,17 +179,17 @@ app.get('/home', async (req, res) => {
     if (req.isAuthenticated()) {
         // The user is logged in
         let journeyObj = await JourneyDB.find({});
-        res.render('userHome', { journeyObj })
+      res.render('userHome',{journeyObj});
 
     } else {
         let journeyObj = await JourneyDB.find({});
-        res.render('home', { journeyObj })
-        // The user is logged out
+        res.render('home')
+        // The user is logged out 
     }
 })
 // 404 page !!
 app.get('*', (req, res) => {
-    req.render('404')
+    res.render('404')
 })
 
 
